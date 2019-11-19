4f4b23a3cab79c9a376028ec41d048493cd291df
crpineda1 <carlos.r.pineda@gmail.com>
2019-11-19 08:47:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        'My name is Carlos'
+    end
+
+    get '/hometown' do
+        'My hometown is Guatemala'
+    end
+
+    get '/favorite-song' do
+        'My favorite song is blah'
+    end
 end
